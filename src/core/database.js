bbffccd946e299c5d0e52116e04cbbc597d7ab19
Colpad <firshol729@gmail.com>
2026-03-28 19:59:58 +0700

    Update database.js

--- src/core/database.js
+++ src/core/database.js
@@ -1,19 +1,43 @@
+// File: src/core/database.js
+
 import fs from "fs"
 
 const path = "./database/database.json"
 
-// tambahkan di atas
-export function getCaptcha() {
-  return db.captcha || (db.captcha = {})
-}
+let db = { groups: {}, users: {} }
 
 export function loadDB() {
-  if (!fs.existsSync(path)) {
-    fs.writeFileSync(path, JSON.stringify({ users: {}, settings: {} }, null, 2))
+  if (fs.existsSync(path)) {
+    db = JSON.parse(fs.readFileSync(path))
   }
-  return JSON.parse(fs.readFileSync(path))
 }
 
-export function saveDB(db) {
+export function saveDB() {
   fs.writeFileSync(path, JSON.stringify(db, null, 2))
 }
+
+export function getGroup(jid) {
+  if (!db.groups[jid]) {
+    db.groups[jid] = {
+      antiLink: false,
+      welcome: false,
+      antiSpam: false,
+      badword: false
+    }
+  }
+  return db.groups[jid]
+}
+
+export function getUser(id) {
+  if (!db.users[id]) {
+    db.users[id] = {
+      money: 1000,
+      exp: 0,
+      level: 1,
+      lastDaily: 0
+    }
+  }
+  return db.users[id]
+}
+
+export default db
